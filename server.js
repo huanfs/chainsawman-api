4c2ce6e6ada953369dff547d257d6b2b18d95376
huanfs <huanfiguereido@gmail.com>
2026-06-19 13:30:21 -0300

    :pencil2: fix: corrige a importação da rota play

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import { Episodes } from "./models/episodes.js";
 import Register from "./routes/register.js";
 import Authenticate from "./routes/authenticate.js";
 import Remove from "./routes/remove.js";
-import Play from "./router/play.js";
+import Play from "./routes/play.js";
 export const server = express();
 server.use(cors({
     origin: "*",
@@ -15,7 +15,7 @@ server.use(bodyParser.json());
 server.use("/register", Register);
 server.use("/authenticate", Authenticate);
 server.use("/remove", Remove);
-server,use("/play", Play);
+server.use("/play", Play);
 server.listen(3000, ()=>{
     console.log("servidor rodando");
 });
